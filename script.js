30b0a97883ed3b3b8b383101cdcbdd186a656357
Arron-B <arronmail@yahoo.com>
2023-09-23 15:02:14 +0100

    basic layout and interactivity implemented

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 ///////  Selectors ///////////
-const monthButton = document.querySelector(".selected-month");
 const tasksBtn = document.getElementById("tab-button-1");
 const completedBtn = document.getElementById("tab-button-2");
 const categoriesBtn = document.getElementById("tab-button-3");
 const tabButtons = document.querySelectorAll(".tab-button");
-
 const taskTab = document.getElementById("tab1");
 const completedTab = document.getElementById("tab2");
 const categoriesTab = document.getElementById("tab3");
@@ -12,20 +10,40 @@ const allTabs = document.querySelectorAll(".tab");
 const activeTab = document.getElementsByClassName("active-tab")[0];
 
 const taskLists = document.getElementsByClassName("task-list-buttons")[0];
-const selectedListBtn = document.getElementsByClassName("selected-list")[0];
 
+const taskModalBtn = document.querySelector(".new-task-button");
+const taskModal = document.querySelector(".task-modal");
+const taskModalInner = document.querySelector(".task-modal-container");
+const xTaskModBtn = document.querySelector(".close-task-modal");
+
+const listModalBtn = document.querySelector(".add-new-list");
 ///////// Variables //////////
+
+let page = 1;
+
+const categories = {
+  Work: 'Work',
+  Chores: 'Chores',
+  Learning: 'Learning',
+  Health: 'Health',
+  Family: 'Family',
+  Friends: 'Friends',
+  Leisure: 'Leisure',
+  Holiday: 'Holiday',
+  Finance: 'Finance',
+  Other: 'Other'
+};
 
 const tasks = {
   allTasks : [],
-  completedTasks: []
-  /* exTaskList : {
-       listName : 'string',
-       tasks : [array]
-  } */
-}
+};
+
+//task ex
+// {task: str,   category: str, complete: false}
 
 /////// function declarations ////////////
+
+// const optionElement = document.createElement("option");
 
 function showTab(selectedTab, clickedButton) {
   allTabs.forEach((el) => {
@@ -43,8 +61,19 @@ function showTab(selectedTab, clickedButton) {
   clickedButton.classList.add("active-button");
 }
 
+
+// function updateCategories () {
+//   for (const category in categories) {
+    
+//   }
+// }
+
+/////// function invokes //////////
+
+
 /////// event listeners  /////////
 
+//Buttons
 tasksBtn.addEventListener("click", () => {
   showTab(taskTab, tasksBtn);
 });
@@ -57,12 +86,19 @@ categoriesBtn.addEventListener("click", () => {
   showTab(categoriesTab, categoriesBtn);
 });
 
-selectedListBtn.addEventListener("click", () => {
-  taskLists.classList.toggle('show');
-});
 
-document.addEventListener("click", (event) => {
-  if (!event.target.closest('.selected-list') && taskLists.classList.contains('show')) {
-    taskLists.classList.remove('show')
-  }
-});
+// Modals
+
+taskModalBtn.addEventListener("click", () => {
+  taskModal.showModal();
+})
+
+xTaskModBtn.addEventListener("click", () => {
+  taskModal.close();
+})
+
+taskModal.addEventListener('click', () => {
+  taskModal.close()
+})
+
+taskModalInner.addEventListener('click', (event) => event.stopPropagation());
